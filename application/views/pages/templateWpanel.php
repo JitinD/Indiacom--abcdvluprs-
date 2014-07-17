bb5ff9ebc65256f51fb3353c61d32dffa677050e
Saurabh Sharma <lavishlibra0810@gmail.com>
2014-07-17 11:56:14 +0530

    About INDIACom page updated

---
+++ application/views/pages/templateWpanel.php
@@ -0,0 +1,21 @@
+<div class="row contentBlock-top">
+    <div class="col-md-2 col-sm-4 col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+
+            </div>
+            <ul class="list-group">
+
+                <li class="list-group-item"><a href="#"></a></li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="col-md-9 col-sm-8 col-xs-12">
+    <span class="h1 text-theme">INDIACom</span>
+    <div class="row h4">
+        <div class="col-md-12">
+        </div>
+    </div>
+</div>
---
+++ application/views/pages/templatel.php
@@ -0,0 +1,7 @@
+<div class="col-md-9 col-sm-8 col-xs-12">
+    <span class="h1 text-theme">INDIACom</span>
+    <div class="row h4">
+        <div class="col-md-12">
+        </div>
+    </div>
+</div>
