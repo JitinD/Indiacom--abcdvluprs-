41ef8b066e9ecc82264495afda989ec3468fee30
Saurabh Sharma <lavishlibra0810@gmail.com>
2014-07-18 13:16:59 +0530

    submit paper (incomplete)

---
+++ application/views/pages/submitpaper.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-9 col-sm-8 col-xs-12">
+            <span class="h1 text-theme">Submit Paper</span>
+            <div class="row body-text">
+                <div class="col-md-12">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
