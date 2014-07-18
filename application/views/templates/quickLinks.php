d5c0ff83ce9ce86eb5a99aae6a681f7c3f08f2c8
Saurabh Sharma <lavishlibra0810@gmail.com>
2014-07-18 20:57:20 +0530

    Quicklinks Bug Fixes
    Search bar integrated within quicklinks bar at all screen sizes.

--- application/views/templates/quickLinks.php
+++ application/views/templates/quickLinks.php
@@ -8,7 +8,7 @@
 ?>
 
 <div class="row border-bottom contentBlock-top bg-dark-blue text-light">
-    <div class="col-md-12 text-center">
+    <div class="col-md-10 col-sm-10 col-xs-12 text-center">
         <ul class="list-inline" style="font-size: 16px;">
             <span class="h4 text-muted">Quick Links &raquo;</span
             <li>
@@ -24,14 +24,16 @@
             <li>
                 <a href="signup" class="text-light">Register</a>
             </li>
-
-            <form role="search" class="form-inline navbar-right">
-                <div class="form-group input-sm">
-                    <input type="text" placeholder="Search" class="form-control">
-                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-                </div>
-
-            </form>
         </ul>
+    </div>
+    <div class="col-md-2 col-sm-2 col-xs-12">
+        <form role="search">
+            <div class="input-group input-group-sm">
+                <input type="text" class="form-control input-sm" placeholder="Search" name="srch-term" id="srch-term">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
