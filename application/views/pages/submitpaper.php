51165f950c3bd68dcd5663f77de2f6e7c95385c5
sauravdebp <sauravdebp@gmail.com>
2014-07-20 11:23:18 +0530

    Submit paper form structure

--- application/views/pages/submitpaper.php
+++ application/views/pages/submitpaper.php
@@ -4,9 +4,80 @@
             <span class="h1 text-theme">Submit Paper</span>
             <div class="row body-text">
                 <div class="col-md-12">
-                    
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label for="name" class="col-sm-3 control-label">Paper Title</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="paper_title" placeholder="Enter paper title">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-3 control-label">Event</label>
+                            <div class="col-sm-9">
+                                <select class="form-control" id="category">
+
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-3 control-label">Track</label>
+                            <div class="col-sm-9">
+                                <select class="form-control" id="category">
+
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-3 control-label">Subject</label>
+                            <div class="col-sm-9">
+                                <select class="form-control" id="category">
+
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="biodata" class="col-sm-3 control-label">Upload Paper(.docx, .doc)</label>
+                            <div class="col-sm-9">
+                                <input type="file" class="form-control" id="biodata" placeholder="Choose File">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="biodata" class="col-sm-3 control-label">Authors</label>
+                            <div class="col-sm-9">
+                                <div id="authorList">
+                                    <div>
+                                        <input type="radio" name="mainAuthor">
+                                        <input type="text" placeholder="Author Name">
+                                    </div>
+                                </div>
+                                <div>
+                                    <input type="button" value="Add another author" id="but_addAuthor">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+    var noofAuth = 1;
+    $(document).ready(function()
+    {
+        $("#but_addAuthor").click(function()
+        {
+            var html =  "<div>" +
+                "<input type=\"radio\" name=\"mainAuthor\"> " +
+                "<input type=\"text\" placeholder=\"Author Name\">" +
+                "<input type=\"button\" value=\"Remove\" class=\"but_remove\">" +
+                "</div>";
+            $("#authorList").append(html);
+            $(".but_remove").click(function()
+            {
+                $(this).parentsUntil("#authorList").remove();
+            });
+        });
+    });
+</script>
