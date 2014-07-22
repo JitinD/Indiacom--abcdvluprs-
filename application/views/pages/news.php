c637f57e95710397b7368c4c2227b174c6ee3781
Saurabh Sharma <lavishlibra0810@gmail.com>
2014-07-22 11:33:11 +0530

    News Page added, Navbar updated, contact us updated

---
+++ application/views/pages/news.php
@@ -0,0 +1,39 @@
+<div class="row contentBlock-top">
+    <div class="col-md-2 col-sm-4 col-xs-12">
+       <?php include('importantdatesPanel.php'); ?>
+    </div>
+    <div class="col-md-9 col-sm-8 col-xs-12">
+        <span class="h1 text-theme">News</span>
+        <div class="row h4">
+            <div class="col-md-12">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>News</th>
+                        <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td><a href="#">Call for Papers: INDIACom-2015</a></td>
+                        <td>26<sup>th</sup> July, 2014</td>
+
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Plagiarism Policy: INDIACom-2015</td>
+                        <td>26<sup>th</sup> July, 2014</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Papers presented during INDIACom - 2014, are now available online at IEEE Xplore</td>
+                        <td>26<sup>th</sup> July, 2014</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
