c048620c5cab069623cf4e6883ca65b03463455b
Saurabh Sharma <lavishlibra0810@gmail.com>
2014-07-23 12:16:22 +0530

    dashboard template

---
+++ application/views/pages/dashboard.php
@@ -0,0 +1,14 @@
+<div class="row contentBlock-top">
+    <div class="col-md-2 col-sm-4 col-xs-12">
+        <?php include('dashboardPanel.php');?>
+        <?php include('importantdatesPanel.php');?>
+    </div>
+
+    <div class="col-md-9 col-sm-8 col-xs-12">
+        <span class="h1 text-theme">Dashboard</span>
+        <div class="row">
+           <?php include('dashboardHome.php');?>
+        </div>
+    </div>
+</div>
+
