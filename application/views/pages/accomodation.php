0c8ed81f181bfa84e1ff65df30a4b1a4875d2a4f
Saurabh Sharma <lavishlibra0810@gmail.com>
2014-07-26 21:30:28 +0530

    Indiacom2015 Accomodation

--- application/views/pages/accomodation.php
+++ application/views/pages/accomodation.php
@@ -4,8 +4,138 @@
     </div>
     <div class="col-lg-9 col-md-8 col-sm-8 col-xs-12">
         <span class="h1 text-theme">Accomodation</span>
-        <div class="row h4">
-            <div class="col-md-12">
+        <hr>
+        <div class="row body-text">
+            <div class="col-md-12 text-justify">
+                <p>Authors / Delegates are requested to kindly contact the below mentioned hotels directly for booking of accommodation as per their arrival / departure schedule.</p>
+                <p>Request for the <strong>applicable special discount</strong> on behalf of <strong>Bharati Vidyapeeth, New Delhi (INDIA)</strong> on account of <strong>INDIACom -2015</strong>.</p>
+                <p>As a <strong>special case for international participants</strong>, on advance payment basis, accommodation can be provided, by our Local Organizing Committee, at subsidized rates, at India International Centre (IIC), Lodi Road, New Delhi </span>and nearby hotels, based on the request. For more information e-mail us at  <a href="mailto:indiacom2015@gmail.com">indiacom2015@gmail.com</a>.</p>
+                <table class="table table-responsive table-hover">
+                    <thead>
+                        <tr>
+                            <th>Hotel <br> & Contact Information</th>
+                            <th>Category</th>
+                            <th>Tariff (INR)</th>
+                            <th>Distance from Venue</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td>
+                            <strong>RADISSON BLU </strong><br>
+                            Plot No.    D, District Centre<br>
+                            Outer Ring Road, Paschim Vihar, <br>
+                            New Delhi-110063<br>
+                            <strong>Ph</strong>: +91-11-46399999<br>
+                            <strong>Web</strong>: <a href="http://www.radissonblu.com">www.radissonblu.com</a><br>
+                            <strong>e-mail</strong>: <a href="mailto:dosm@rdpvdelhi.com">dosm@rdpvdelhi.com</a>
+                        </td>
+                        <td>5 <span class="glyphicon glyphicon-star"></span></td>
+                        <td>4,400-15,000 +Taxes</td>
+                        <td>3 km</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong><em>CITY PARK</em></strong><br>
+                            Pitam Pura, <br>
+                            New Delhi-110088<br>
+                            <strong>Ph</strong>: +91-11-42310101 <br>
+                            <strong>Fax</strong>: +91-11-42310100<br>
+                            <strong>Mobile</strong>: +91-9871390472<br>
+                            <strong>Web</strong>:
+                            <a target="_blank" href="http://www.hotelcitypark.in">
+                                www.hotelcitypark.in</a>,
+                            <br>
+                            <a target="_blank" href="http://www.hotelcitypark.com">www.hotelcitypark.com</a><br>
+                            <strong>e-mail</strong>: <a href="mailto:hotelcitypark@vsnl.net">hotelcitypark@vsnl.net</a>
+                        </td>
+                        <td>4 <span class="glyphicon glyphicon-star"></span> Deluxe</td>
+                        <td>4,500-14,000 +Taxes</td>
+                        <td>10 km</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong><em>SWIFT INN</em></strong><br>
+                            397, Bhera Enclave, <br>
+                            Paschim Vihar, <br>
+                            Main Outer Ring Road, <br>
+                            Opposite Behera Enclave Round Circle, <br>
+                            New Delhi-110087<br>
+                            <strong>Ph</strong>: +91-11-41557348,
+                            +91-11-25289342<br>
+                            <strong>Mobile</strong>: +91-9716104200<br>
+                            <strong>Web</strong>:
+                            <a target="_blank" href="http://www.hotelswiftinn.in">www.hotelswiftinn.in</a><br>
+                            <strong>e-mail</strong>: <a href="mailto:hotelswiftinn@yahoo.com">hotelswiftinn@yahoo.com</a>
+
+                        </td>
+                        <td>3 <span class="glyphicon glyphicon-star"></span></td>
+                        <td>2,500-2,800 +Taxes</td>
+                        <td>2 km</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong><em>AMAN PALACE</em></strong><br>
+                            A-40, Vishal Enclave, <br>
+                            Rajouri Garden, <br>
+                            Opp. TDI Mall, <br>
+                            New Delhi &ndash; 110027<br>
+                            <strong>Ph</strong>: +91-11-25921555, <br>
+                            +91-11-25113555,<br>
+                            +91-11-25160111 <br>
+                            <strong>Fax</strong>: +91-11-25160111<br>
+                            <strong>Web</strong>:
+                            <a target="_blank" href="http://www.hotelamanpalace.com">www.hotelamanpalace.com</a><br>
+                            <strong>e-mail</strong>:
+                            <a href="mailto:reservation@hotelamanpalace.com">reservation@hotelamanpalace.com</a>, <br>
+                            <a href="mailto:hotelamanpalace@vsnl.net">hotelamanpalace@vsnl.net</a>
+                        </td>
+                        <td>3 <span class="glyphicon glyphicon-star"></span></td>
+                        <td>1,500-2,250</td>
+                        <td>3 km</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong><em>AMAN DELUXE</em></strong><br>
+                            A-30-31, Vishal Enclave, <br>
+                            Opposite Vishal Cinema, <br>
+                            Rajouri Garden, <br>
+                            New Delhi &ndash; 110027<br>
+                            <strong>Ph</strong>: 011-45702658,<br>
+                            +91-11-45024327, <br>
+                            +91-11-25934555, <br>
+                            +91-11-25421555<br>
+                            <strong>Mobile</strong>: +91-9555104555,<br>
+                            +91-9311045777<br>
+                            <strong>Web</strong>: <a href="http://www.hotelamandeluxe.com">www.hotelamandeluxe.com</a><br>
+                            <strong>e-mail</strong>:
+                            <a href="mailto:hotelamandeluxe@gmail.com">hotelamandeluxe@gmail.com</a>
+                        </td>
+                        <td>3 <span class="glyphicon glyphicon-star"></span></td>
+                        <td>2,300-3,500</td>
+                        <td>3 km</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>SWATI </strong><br>
+                            25, Paschim Vihar Extension, <br>
+                            Main Rohtak Road, <br>
+                            Opposite Metro Pillar No. 195, <br>
+                            New Delhi-110063<br>
+                            <strong>Ph</strong>: +91-11-25218000, <br>
+                            +91-11-25218001<br>
+                            <strong>Mobile</strong>: +91-9211727127,
+                            +91-9211727129<br>
+                            <strong>Web</strong>: <a href="http://www.swatihotels.com">www.swatihotels.com</a><br>
+                            <strong>e-mail</strong>:
+                            <a href="mailto:paschimvihar@hotelswati.com">paschimvihar@hotelswati.com</a>
+                        </td>
+                        <td>3 <span class="glyphicon glyphicon-star"></span></td>
+                        <td>2,500-5,000 +Taxes</td>
+                        <td>Walking</td>
+                    </tr>
+                </table>
+
+
             </div>
         </div>
     </div>
