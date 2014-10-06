73d48cf42c198c3771846437e9e8a8fecff7cfb4
lavishlibra0810 <lavishlibra0810@gmail.com>
2014-10-06 17:08:09 +0530

    AddNews view split in 2

--- bvicam_admin/application/views/pages/addNews.php
+++ bvicam_admin/application/views/pages/addNews.php
@@ -32,24 +32,6 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="attachmentName" class="col-sm-3 control-label">Attachment Name</label>
-                    <div class="col-sm-6">
-                        <input type="text" name = "attachmentName" class="form-control" id="attachmentName">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="attachmentUrl" class="col-sm-3 control-label">Attachment URL</label>
-                    <div class="col-sm-6">
-                        <input type="file" name = "attachmentUrl" class="form-control" id="attachmentUrl">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="stickyDate" class="col-sm-3 control-label">Sticky Date</label>
-                    <div class="col-sm-6">
-                        <input type="date" name = "content" class="form-control" id="stickyDate">
-                    </div>
-                </div>
-                <div class="form-group">
                     <div class="col-sm-6 col-sm-offset-3">
                         <button type="submit" class="btn btn-success">Submit</button>
                         <button type="reset" class="btn btn-danger">Clear</button>
---
+++ bvicam_admin/application/views/pages/addNewsIndiacom.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <form class="form-horizontal" enctype="multipart/form-data" method = "post">
+    <div class="form-group">
+    <label for="attachmentName" class="col-sm-3 control-label">Attachment Name</label>
+    <div class="col-sm-6">
+        <input type="text" name = "attachmentName" class="form-control" id="attachmentName">
+    </div>
+</div>
+<div class="form-group">
+    <label for="attachmentUrl" class="col-sm-3 control-label">Attachment URL</label>
+    <div class="col-sm-6">
+        <input type="file" name = "attachmentUrl" class="form-control" id="attachmentUrl">
+    </div>
+</div>
+<div class="form-group">
+    <label for="stickyDate" class="col-sm-3 control-label">Sticky Date</label>
+    <div class="col-sm-6">
+        <input type="date" name = "content" class="form-control" id="stickyDate">
+    </div>
+</div>
+<div class="form-group">
+    <div class="col-sm-6 col-sm-offset-3">
+        <button type="submit" class="btn btn-success">Submit</button>
+        <button type="reset" class="btn btn-danger">Clear</button>
+    </div>
+</div>
+</form>
