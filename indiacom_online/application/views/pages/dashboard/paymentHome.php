cf01963a6a9c71e88b9b097cd06dfac688f10b51
lavishlibra0810 <lavishlibra0810@gmail.com>
2015-01-18 13:05:20 +0530

    Form complete sort of. AJAX needs to be implemented

--- indiacom_online/application/views/pages/dashboard/paymentHome.php
+++ indiacom_online/application/views/pages/dashboard/paymentHome.php
@@ -2,6 +2,7 @@
     <h3 class="text-theme">Payments</h3>
     <hr>
     <form class="form-horizontal">
+
         <div id="step1">
 
             <span class="h3 text-primary">
@@ -25,12 +26,63 @@
                     <td></td>
                     <td>
                         <div class="btn-group">
-                            <label class="btn btn-info">
+                            <label class="btn btn-info brcheck">
+                                <input type="radio" name="brorep" autocomplete="off"> Basic Registration
+                            </label>
+                        </div>
+                        <div class="btn-group">
+                            <label class="btn btn-info epcheck">
+                                <input type="radio" name="brorep" autocomplete="off"> Extra Paper
+                            </label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="btn-group">
+                            <label class="btn btn-info brcheck">
                                 <input type="checkbox" autocomplete="off"> Basic Registration
                             </label>
                         </div>
                         <div class="btn-group">
-                            <label class="btn btn-info">
+                            <label class="btn btn-info epcheck">
+                                <input type="checkbox" autocomplete="off"> Extra Paper
+                            </label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="btn-group">
+                            <label class="btn btn-info brcheck">
+                                <input type="checkbox" autocomplete="off"> Basic Registration
+                            </label>
+                        </div>
+                        <div class="btn-group">
+                            <label class="btn btn-info epcheck">
+                                <input type="checkbox" autocomplete="off"> Extra Paper
+                            </label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="btn-group">
+                            <label class="btn btn-info brcheck">
+                                <input type="checkbox" autocomplete="off"> Basic Registration
+                            </label>
+                        </div>
+                        <div class="btn-group">
+                            <label class="btn btn-info epcheck">
                                 <input type="checkbox" autocomplete="off"> Extra Paper
                             </label>
                         </div>
@@ -38,45 +90,21 @@
                 </tr>
                 </tbody>
             </table>
-            <div>
-                <button class="btn btn-success" id="showForMore">
-                    Pay for Other Authors &nbsp;<span class="glyphicon glyphicon-plus"></span>
-                </button>
-            </div>
             <div id="forMoreAuthors">
-
                 <div class="contentBlock-top">
-                    <span class="h3 text-primary">
-                        Outstanding Amount for Member ID XY : xyz
-                    </span>
-                    <div class="contentBlock-top">
-                        <table class="table table-responsive table-striped table-hover">
-                            <thead>
-                            <tr>
-                                <th>Paper ID</th>
-                                <th>Paper Title</th>
-                                <th>Pending Amount</th>
-                                <th>Type</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>
-                                    <div class="btn-group">
-                                        <label class="btn btn-info">
-                                            <input type="checkbox" autocomplete="off"> Basic Registration
-                                        </label>
-                                    </div>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
+
+
+
                 </div>
+            </div>
+            <div id="addHere">
+
+            </div>
+
+            <div class="contentBlock-top">
+                <button type="button" class="btn btn-success" id="addAuthors">
+                    Pay for Another Author &nbsp;<span class="glyphicon glyphicon-plus"></span>
+                </button>
             </div>
             <hr>
         <span class="h2 text-danger">
@@ -86,6 +114,7 @@
         <div id="step2">
             <div class="form-group">
                 <label class="col-sm-3" for="paymentMode">Add Mode of Payment</label>
+
                 <div class="col-sm-6">
                     <select id="paymentMode" class="form-control">
                         <option>Cheque</option>
@@ -95,12 +124,14 @@
             </div>
             <div class="form-group">
                 <label class="col-sm-3" for="transno">Cheque No / DD No / Wired Trans ID</label>
+
                 <div class="col-sm-6">
                     <input id="transno" type="text" class="form-control">
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-sm-3" for="transdate">Cheque Date / DD Date / Wired Trans Date</label>
+
                 <div class="col-sm-6">
                     <input id="transdate" type="date" class="form-control">
                 </div>
@@ -113,45 +144,77 @@
         </div>
     </form>
     <div class="contentBlock-top">
-        <button class="btn btn-lg btn-primary" id="showStep1">
+        <button class="btn btn-primary" id="showStep1">
             Previous <span class="glyphicon glyphicon-chevron-left"></span>
         </button>
-        <button class="btn btn-lg btn-primary" id="showStep2">
+        <button class="btn btn-primary" id="showStep2">
             Next <span class="glyphicon glyphicon-chevron-right"></span>
         </button>
     </div>
 </div>
 <script>
-    $(document).ready(function()
-    {
+    $(document).ready(function () {
         $("#step1").show();
         $("#step2").hide();
-        $("#forMoreAuthors").hide();
+        $("#showStep1").hide();
 
-        $("#showStep1").click(function()
-        {
-           $("#step1").show(500);
+        $("#showStep1").click(function () {
+            $("#step1").show(500);
             $("#step2").hide(500);
+            $("#showStep1").hide();
+            $("#showStep2").show();
         });
 
-        $("#showStep2").click(function()
-        {
+        $("#showStep2").click(function () {
             $("#step2").show(500);
             $("#step1").hide(500);
+            $("#showStep1").show();
+            $("#showStep2").hide();
         });
 
-        $("#showForMore").click(function()
-            {
-                var html =  "<div>" +
-                    "<input type=\"radio\" name=\"main_author\" class=\"main_author\"> " +
-                    "<input type=\"text\" name=\"authors[]\" class=\"authors\" placeholder=\"Author Id\">" +
-                    "<button type=\"button\"  value=\"Remove\" class=\"but_remove btn btn-sm btn-danger\">" +
-                    "<span class=\"glyphicon glyphicon-minus\">" + "</span>" +
-                    "</button>" +
-                    "</div>";
-                $("forMoreAuthors").append(html);
-            }
-        );
+        $("#addAuthor").click(function () {
+
+        });
+        $("#addAuthors").click(function(){
+            var html1=
+            "<span class=\"h3 text-primary\">"+
+            "Outstanding Amount for Member ID XY : xyz"+
+            "</span>";
+
+            var html2=
+            "<div class=\"contentBlock-top\">"+
+            "<table class=\"table table-responsive table-striped table-hover\">"+
+            "<thead>"+
+            "<tr>"+
+            "<th>Paper ID</th>"+
+            "<th>Paper Title</th>"+
+            "<th>Pending Amount</th>"+
+            "<th>Type</th>"+
+            "<th></th>"+
+            "</tr>"+
+            "</thead>"+
+            "<tbody>"+
+            "<tr>"+
+            "<td></td>"+
+            "<td></td>"+
+            "<td></td>"+
+            "<td>"+
+            "<div class=\"btn-group\">"+
+            "<label class=\"btn btn-info\">"+
+            "<input type=\"checkbox\" autocomplete=\"off\"> Basic Registration"+
+            "</label>"+
+            "</div>"+
+            "</td>"+
+            "</tr>"+
+            "</tbody>"+
+            "</table>"+
+            "</div>";
+            var html = html1+html2;
+
+            $("#addHere").append(html);
+        });
+
     });
+
 
 </script>
