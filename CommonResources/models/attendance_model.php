4f9990e506c7e9b56461d6790829d4fe677510ce
pavithrag11 <pavithra.g11@gmail.com>
2015-02-17 12:45:47 +0530

    Attendance Manager and Model restructured

--- CommonResources/models/attendance_model.php
+++ CommonResources/models/attendance_model.php
@@ -9,6 +9,7 @@
 class Attendance_model extends CI_Model
 {
     public $error;
+    private $entity = "attendance_master";
 
     public function __construct()
     {
@@ -16,35 +17,52 @@ class Attendance_model extends CI_Model
         $this->load->database();
     }
 
-    private function assignId($entity)
+    private function assignId()
     {
-        $sql = "SELECT max(cast(`member_id` as UNSIGNED))as `attendance_id` from $entity";
+        //$sql = "SELECT max(cast(`member_id` as UNSIGNED))as `attendance_id` from $entity";
+        /*$sql = "SELECT `attendance_id` from $this -> entity";
+
         $query = $this->db->query($sql);
+
         if($query->num_rows()==0)
             return 1;
+
         $attendance_id_array = $query->row_array();
-        $attendance_id = $attendance_id_array['attendance_id'] + 1;
+        $attendance_id = $attendance_id_array['attendance_id'] + 1;*/
+
+        $sql = "SELECT max(`attendance_id`) from $this -> entity";
+
+        $query = $this->db->query($sql);
+
+        if($query->num_rows()==0)
+            return 1;
+
+        $attendance_id_object = $query->row();
+        $attendance_id = $attendance_id_object -> attendance_id;
+
         return $attendance_id;
     }
 
 
-    public function markDeskAttendance($entity,$attendanceRecord=array())
+    public function markDeskAttendance($attendanceRecord=array())
     {
-        $attendanceRecord['attendance_id']=$this->assignId($entity);
-        $is_marked=$this->checkDeskAttendance($attendanceRecord['submission_id']);
-        if($is_marked==null)
+        $record = $this->getAttendanceRecord($attendanceRecord['submission_id']);
+
+        if($record==null)
         {
-            $this -> db -> insert($entity, $attendanceRecord);
+            $attendanceRecord['attendance_id']=$this->assignId();
+            $this -> db -> insert($this -> entity, $attendanceRecord);
         }
         else
         {
-            $this -> db -> update($entity,$attendanceRecord);
+            $this -> db -> where('attendance_id', $attendanceRecord -> attendance_id);
+            $this -> db -> update($this -> entity, $attendanceRecord);
         }
 
         return $this->db->trans_status;
     }
 
-    public function checkDeskAttendance($submission_id)
+    public function getAttendanceRecord($submission_id)
     {
         $sql="Select * from
               attendance_master
@@ -56,9 +74,10 @@ class Attendance_model extends CI_Model
             return null;
         return $query->row();
     }
-    public function markTrackAttendance($entity,$attendanceRecord=array())
+
+    public function markTrackAttendance($attendanceRecord=array())
     {
-        $this -> db -> update($entity,$attendanceRecord);
+        $this -> db -> update($attendanceRecord);
         return $this->db->trans_status;
     }
 
--- bvicam_admin/application/controllers/AttendanceManager.php
+++ bvicam_admin/application/controllers/AttendanceManager.php
@@ -16,30 +16,36 @@ class AttendanceManager extends CI_Controller
 
     }
 
-    public function MarkDeskAttendance($member_id, $paper_id, $is_present)
+    public function markDeskAttendance($member_id, $paper_id, $is_present)
     {
         $this->load->model("attendance_model");
         $this->load->model("submission_model");
+
         $submission_id_array = $this->submission_model->getSubmissionID($member_id, $paper_id);
         $submission_id = $submission_id_array->submission_id;
-        $attendance_record = array(
+
+        $attendanceRecord = array(
             'event_id' => EVENT_ID,
             'member_id' => "",
             'submission_id' => $submission_id,
             'is_present_on_desk' => $is_present
         );
-        $this->attendance_model->markDeskAttendance("attendance_master", $attendance_record);
+
+        $this->attendance_model->markDeskAttendance($attendanceRecord);
     }
 
-    public function MarkTrackAttendance($member_id, $paper_id, $is_present)
+    public function markTrackAttendance($member_id, $paper_id, $is_present)
     {
         $this->load->model("attendance_model");
         $this->load->model("submission_model");
+
         $submission_id_array = $this->submission_model->getSubmissionID($member_id, $paper_id);
         $submission_id = $submission_id_array->submission_id;
-        $track_attendance = $this->attendance_model->checkDeskAttendance($submission_id);
+
+        $track_attendance = $this->attendance_model->getAttendanceRecord($submission_id);
+
         if ($track_attendance->is_present_on_desk == 1) {
-            $desk_attendance['is_present_in_hall'] = $is_present;
+            $track_attendance['is_present_in_hall'] = $is_present;
             return json_encode($this->attendance_model->markTrackAttendance("attendance_master", $track_attendance));
         }
         else {
