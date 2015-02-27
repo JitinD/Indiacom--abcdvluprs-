c0b7dab61ff0faf0dc6ca54457d0209415b0c7fd
lavishlibra0810 <lavishlibra0810@gmail.com>
2015-02-27 19:46:59 +0530

    Sidebar Added

--- bvicam_admin/application/views/templates/navbar.php
+++ bvicam_admin/application/views/templates/navbar.php
@@ -1,10 +1,15 @@
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="container-fluid">
         <div class="col-md-8 col-sm-8 col-xs-8">
+            <button type="button" class="btn btn-default navbar-btn" data-toggle="offcanvas" data-target="#myNavmenu"
+                    data-canvas="body">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+            </button>
             <a data-toggle="modal" data-target="#myModal">
                 <img src="/<?php echo PATH ?>assets/images/bvicamlogo.png" class="btn btn-lg" style="height: 60px; ;">
             </a>
             <span class="btn">BVICAM Admin System</span>
+
         </div>
         <div class="nav navbar-nav navbar-right">
             <?php
@@ -22,3 +27,19 @@
         </div>
     </div>
 </div>
+<nav id="myNavmenu" class="navmenu navmenu-inverse navmenu-fixed-left offcanvas" role="navigation">
+    <ul class="nav navmenu-nav">
+        <li class="active"><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+            <ul class="dropdown-menu navmenu-nav" role="menu">
+                <li><a href="sidebar.php">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
