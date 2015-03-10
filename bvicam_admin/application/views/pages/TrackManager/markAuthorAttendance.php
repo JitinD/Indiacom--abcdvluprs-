a88721a1917bd3e5b3b2dfbaefc7fc790cd758f3
JitinD <jitin.dominic01@gmail.com>
2015-03-10 15:28:45 +0530

    Little Bug fixed

--- bvicam_admin/application/views/pages/TrackManager/markAuthorAttendance.php
+++ bvicam_admin/application/views/pages/TrackManager/markAuthorAttendance.php
@@ -290,7 +290,7 @@
 
                                         if (isset($certificate[$paper->paper_id]['certificate_outward_number']) &&
                                             ($certificate[$paper->paper_id]['certificate_outward_number'] != '') &&
-                                            (isset($attendance[$paper->paper_id]['is_present_in_hall']) && $attendance[$paper->paper_id]['is_present_in_hall'] == 1) && (isset($papersInfo[$paper->paper_id]['pending']) && $papersInfo[$paper->paper_id]['pending'] == 0) &&
+                                            (isset($attendance[$paper->paper_id]['is_present_in_hall']) && $attendance[$paper->paper_id]['is_present_in_hall'] == 1) &&
                                             (isset($certificate[$paper->paper_id]['is_certificate_given']) && ($certificate[$paper->paper_id]['is_certificate_given'] == 1)) && (isset($papersInfo[$paper->paper_id]['paid']) && $pendingAmount == 0))
                                             echo "checked";
                                         ?>
