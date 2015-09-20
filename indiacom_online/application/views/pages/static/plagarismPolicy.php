377365c7e9fe2b313c36fd456a974ae2d194380c
pavithrag11 <pavithra.g11@gmail.com>
2015-09-20 10:20:23 +0530

    Plagarism Policy

---
+++ indiacom_online/application/views/pages/static/plagarismPolicy.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavithra
+ * Date: 20-Sep-15
+ * Time: 10:12
+ */
+?>
+<div class="row contentBlock-top">
+    <div class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+        <?php include('submitPaperSidePanel.php'); ?>
+        <?php include('importantdatesPanel.php'); ?>
+    </div>
+    <div class="col-lg-10 col-md-9 col-sm-9 col-xs-12">
+        <span class="h1 text-theme">Plagarism Policy</span>
+        <hr>
+        <div class="row">
+            <div class="col-md-12 text-justify">
+                Plagiarism is the unethical act of copying someone else’s prior ideas, processes, results or words
+                without
+                explicit acknowledgement of the original author and source. Self-plagiarism occurs when an author
+                utilizes
+                large part of his/her own previously published work without using appropriate references. This can range
+                from getting the same manuscript published in multiple journals to modifying a previously published
+                manuscript with some new data.
+
+                Since its inception in the year 2007, INDIACom has attracted eminent academicians, scientists,
+                researchers,
+                industrialists, technocrats, government representatives, social visionaries and experts from all strata
+                of
+                society, to explore the new horizons of innovations to identify opportunities and define the path
+                forward.
+                This new path is aimed at eliminating isolation, discouraging redundant efforts and promoting scientific
+                progress to accelerate nations’ economic growth to prominence in the international arena; and also at
+                contributing effectively to realize the nations’ vision of sustainable inclusive development using
+                computing. We value Intellectual Property Rights of authors, in case of any dispute related to contents
+                of a
+                manuscript, the first author shall be deemed responsible as the owner of the manuscript.
+
+                Encouraged by the resounding success met with the prior nine editions of INDIACom, 10th INDIACom; 3rd
+                2016
+                International Conference on Computing for Sustainable Global Development; better known as INDIACom-2016
+                has
+                been announced.
+
+                The conference advises its authors, to kindly restrict from copying or plagiarism in any form. Paper
+                submission guidelines clearly specify that the authors should submit their original and unpublished
+                research
+                work not under consideration for publication elsewhere. All manuscripts submitted for publication to
+                INDIACom-2016 will be cross-checked for plagiarism using Turnitin software. Manuscripts found to be
+                plagiarized, during any stage of review, shall not be included in the conference.
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
