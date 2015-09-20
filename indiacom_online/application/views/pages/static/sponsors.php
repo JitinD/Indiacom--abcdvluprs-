b3626abc777859a8cdb767b0959d563ba3f01a7f
pavithrag11 <pavithra.g11@gmail.com>
2015-09-20 10:05:41 +0530

    Sponsers Page
    New Sponsors added

--- indiacom_online/application/views/pages/static/sponsors.php
+++ indiacom_online/application/views/pages/static/sponsors.php
@@ -11,7 +11,8 @@
 
                 <div class="row">
                     <div class="col-md-6 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-2">
-                        <a href="http://www.ewh.ieee.org/r10/delhi/" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/ieee.png"
+                        <a href="http://www.ewh.ieee.org/r10/delhi/" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/ieee.png"
                                 class="sponsorImage img-responsive"></a>
                     </div>
                 </div>
@@ -22,23 +23,128 @@
 
                 <div class="row">
                     <div class="col-md-6 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-2">
-                        <a href="http://www.theiet.org/" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/iet.jpg"
+                        <a href="http://www.theiet.org/" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/iet.jpg"
                                 class="sponsorImage img-responsive">
                             <span>IET (UK), Delhi Local Networks</span></a>
-                        <a href="http://isteonline.in/" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/iste.jpg"
+                        <a href="http://www.csi-india.org/" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/CSI.jpg"
                                 class="sponsorImage img-responsive">
-                            <span>ISTE, Delhi Section</span></a>
-                        <a href="http://www.ipu.ac.in/" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/ipu.jpg"
-                                class="sponsorImage img-responsive">
-                            <span>GGSIP University, New Delhi</span></a>
-                    </div>
-                    <div class="col-md-6 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-2">
-                        <a href="http://iete.org/" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/iete.png"
+                            <span>CSI Region-I & Divisions-I,II,III,IV & V</span></a>
+                        <a href="http://iete.org/" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/IETE.jpg"
                                 class="sponsorImage img-responsive">
                             <span>IETE, Delhi Centre</span></a>
-                        <a href="http://www.csi-india.org/" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/csi-gjlogo.jpg"
+                        <a href="http://isteonline.in/" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/iste.jpg"
                                 class="sponsorImage img-responsive">
-                            <span>CSI Region-I & Division-I</span></a>
+                            <span>ISTE, Delhi Section</span></a>
+                        <a href="http://www.ipu.ac.in/" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/GGSIPU.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>GGSIP University, New Delhi</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/CIRG_logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>Computational Intelligence Research Group </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/NSIT.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>Netaji Subhash Institue of Technology</span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/Jamia Hamdard.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>Jamia University</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/Logo of MSIT.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>Maharaja Surajmal Institute of Technology </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/BPIBS Logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>BPIBS </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/CCET Logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>CCET </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/GBU Logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>Gautam Buddha University </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/Gyancity Logo.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>Gyancity </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/ITM University Logo.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>ITM University </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/IU Logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>Integral University</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/KCT LOGO.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>KUMARAGURU Inst. of Technology </span></a>
+
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/SKITM LOGO.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>SKITM </span></a>
+
+
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/UTU LOGO.png"
+                                                        class="sponsorImage img-responsive">
+                            <span>UKA TARSADIA University  </span></a>
+
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/IISU LOGO.png"
+                                                        class="sponsorImage img-responsive">
+                            <span>The IIS University</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/Juit LOGO.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>JAYPEE University of Information Technology </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/University_LOGO PTU.jpg "
+                                class="sponsorImage img-responsive">
+                            <span>Punjab Technical University </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/fri_logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>Forest Research Institute </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/Organization_LOGO_FCET.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>Ferozepur College of Engineering & Technology</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/GNIM_Logo.gif"
+                                                        class="sponsorImage img-responsive">
+                            <span>Guru Nanak Institute of Management </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/PES.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>People's Education Society</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/RIET.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>Rajasthan Institute of Engineering & Technology</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/DCRUST_logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>DCRUST </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/Dronacharya CoE logo.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>Dronacharya College of Engg. </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/Galgotia University.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>Galgotias University </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/Marwadi Education.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>Marwadi Education Foundation </span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/NIT KKR Logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>National Institute of Technology </span></a>
+                        <a href="" target="_blank"><img
+                                src="/<?php echo PATH ?>assets/images/sponsors/SRM-University-logo.jpg"
+                                class="sponsorImage img-responsive">
+                            <span>SRM University</span></a>
+                        <a href="" target="_blank"><img src="/<?php echo PATH ?>assets/images/sponsors/YMCAUST logo.jpg"
+                                                        class="sponsorImage img-responsive">
+                            <span>YMCA University of Science & Technology </span></a>
+
+
+                    </div>
+                    <div class="col-md-6 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-2">
+
 
                     </div>
                 </div>
